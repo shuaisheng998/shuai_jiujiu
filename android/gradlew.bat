fdd0710cb08759296aa9f4f44fdf0dbc1bb29017
shuaisheng998 <shuaisheng998@users.noreply.github.com>
2026-07-24 01:14:27 +0800

    修复：补gradlew构建入口/删study_record死代码/959条例句/除零保护

---
+++ android/gradlew.bat
@@ -0,0 +1,6 @@
+@rem Gradle wrapper start script for Windows
+@if "%DEBUG%"=="" @echo off
+set DIRNAME=%~dp0
+set APP_HOME=%DIRNAME%
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+"%JAVA_HOME%/bin/java.exe" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
